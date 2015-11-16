af3eead10fb137e5bc0a2e86aa12e047ef6b7085
Oleg Lindvin <oleg.lindvin@gmail.com>
2015-11-16 09:55:18 +0100

    Testing to see if git this works - Oleg

--- app/src/main/java/com/example/iths/asobi/MainActivity.java
+++ app/src/main/java/com/example/iths/asobi/MainActivity.java
@@ -10,6 +10,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    // Hello I am Kozue. I want to change here.
 }
