a42c84cbc87fe648082a7c732c0b205273470895
Hejkitty54 <kozue.kishimoto@.gmail.com>
2015-11-19 12:17:49 +0100

    testing

--- app/src/main/java/com/example/iths/asobi/MainActivity.java
+++ app/src/main/java/com/example/iths/asobi/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.iths.asobi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG,"I am testing");
     }
 
     // Hello I am Kozue. I want to change here.
