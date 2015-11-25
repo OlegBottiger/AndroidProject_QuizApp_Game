68bc8ca4407ba5c7478717a0579fcaeaee0ad81a
Thomas Månsson <thomasmonsson@gmail.com>
2015-11-25 16:20:10 +0100

    Created actionbar and implement home buttom in actionbar exept in mainactivity. Also added icon to drawable and uppdate AndroidManifest

--- app/src/main/java/com/example/iths/asobi/ResultActivity.java
+++ app/src/main/java/com/example/iths/asobi/ResultActivity.java
@@ -1,7 +1,10 @@
 package com.example.iths.asobi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -9,5 +12,53 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+
+        //Set actionbar item
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        setContentView(R.layout.activity_result);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+
+
+        switch (item.getItemId()) {
+            case R.id.action_play:
+                // Play action
+                Intent i = new Intent(ResultActivity.this, GameModeActivity.class);
+                startActivity(i);
+                return true;
+            case R.id.info:
+                // Asobi presentation activity
+                Intent j = new Intent(ResultActivity.this, AboutActivity.class);
+                startActivity(j);
+                return true;
+            case R.id.profile:
+                // Create profile activity
+                Intent k = new Intent(ResultActivity.this, ProfilesActivity.class);
+                startActivity(k);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
     }
 }
