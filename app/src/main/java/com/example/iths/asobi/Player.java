7940fe8cda7d4cf4fc13af199b0ab1fa616755df
Gattanjo Nilsson <gattanjo.nilsson@hotmail.com>
2015-11-25 15:53:57 +0100

    Cursor for profile list  created

--- app/src/main/java/com/example/iths/asobi/Player.java
+++ app/src/main/java/com/example/iths/asobi/Player.java
@@ -6,27 +6,12 @@ package com.example.iths.asobi;
 public class Player {
 
     private final String name;
-    private int score;
 
-    public Player(String name, int score) {
+    public Player(String name) {
         this.name = name;
-        this.score = score;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
     }
 
     public String getName() {
         return name;
     }
-
-    public void increaseScore(int score) {
-        this.score = this.score + score;
-    }
-
 }
