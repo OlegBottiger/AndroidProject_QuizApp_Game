7280ee15b4786d38debac896411ee17af65df571
Hejkitty54 <kozue.kishimoto@.gmail.com>
2015-12-01 17:12:20 +0100

    commit

--- app/src/main/java/com/example/iths/asobi/CustomCategoryActivity.java
+++ app/src/main/java/com/example/iths/asobi/CustomCategoryActivity.java
@@ -11,7 +11,7 @@ public class CustomCategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_category);
-        
+
     }
 
 }
